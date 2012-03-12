cf9deda1fbd27834a77aaa346ccd4afbc820ad35
Simeon Franklin <simeonf@gmail.com>
2012-03-12 14:22:25 -0700

    Add ModelAdmin methods for row-level permissions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from polls.models import Poll
 from polls.models import Choice
@@ -10,7 +11,7 @@ class ChoiceInline(admin.TabularInline):
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields':['question']}),
+        (None, {'fields':['user', 'question']}),
         ('Date Information', {'fields':['pub_date'], 'classes': ['collapse']})
         ]
     inlines = [ChoiceInline]
@@ -19,4 +20,16 @@ class PollAdmin(admin.ModelAdmin):
     search_fields = ['question']
     date_hierarchy = 'pub_date'
 
+    def queryset(self, request):
+        qs = super(PollAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user" and not request.user.is_superuser:
+            kwargs['queryset'] = User.objects.filter(id=request.user.id)
+        return super(PollAdmin, self)\
+                 .formfield_for_foreignkey(db_field, request, **kwargs)
+    
 admin.site.register(Poll, PollAdmin)
